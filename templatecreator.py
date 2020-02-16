83b97cd7c3ddaa5781386ea635e7d35a1e3fef26
Jason Lu <57573649+jlu7000@users.noreply.github.com>
2020-02-15 23:25:56 -0500

    Update templatecreator.py

--- templatecreator.py
+++ templatecreator.py
@@ -1,14 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-# opens chrome/google doc
-driver = webdriver.Firefox(
-    'geckodriver path')
-driver.get('https://docs.google.com/document/u/0/')
-driver.implicitly_wait(10)
-# inputs email
-driver.find_element_by_id('identifierId').send_keys("email")
-driver.find_element_by_id('identifierNext').click()
-driver.find_element_by_name('password').send_keys("password")
-driver.find_element_by_id('passwordNext').click()
 
-print("shitbot")
+isMLA = True
+
+
+# opens chrome/google doc
+driver = webdriver.Chrome(
+    '/Users/jason/OneDrive - University of North Georgia/CSCI 1301/chromedriver')
+driver.get('https://office.live.com/start/word.aspx/')
+driver.implicitly_wait(30)
+# checks if you are logged in
+while True:
+    if driver.current_url == 'https://www.office.com/launch/word?auth=1':
+        break
+#opens new blank doc  
+driver.find_element_by_xpath(
+        '//*[@id="officeHome__content"]/ohp-wac-start/div/div/div[2]/div/div[1]/ohp-template-item/a').click()
+print('shitbot')
+# if its is MLA
+#if isMLA:
+
+def typeMLA(name, subject, professor, due_date):
+    print(name+subject+professor+due_date)
