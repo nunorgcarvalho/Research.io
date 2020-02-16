9658c3e0f0322192905fc4ecd6fc2cd14990aa99
Jason Lu <57573649+jlu7000@users.noreply.github.com>
2020-02-15 20:41:50 -0500

    Update templatecreator.py

--- templatecreator.py
+++ templatecreator.py
@@ -1,3 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+# opens chrome/google doc
+driver = webdriver.Chrome(
+    '/Users/jason/OneDrive - University of North Georgia/CSCI 1301/chromedriver')
+driver.get('https://docs.google.com/document/u/0/')
+driver.implicitly_wait(10)
+# inputs email
+driver.find_element_by_id('identifierId').send_keys("j.lu7000@gmail.com")
+driver.find_element_by_id('identifierNext').click()
+driver.find_element_by_name('password').send_keys("Jl214755!")
+driver.find_element_by_id('passwordNext').click()
 
-print("hello world")
+print("shitbot")
