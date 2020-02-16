821903fafb4b37fbacc1d2c68ef44cf8674ff60e
Jason Lu <57573649+jlu7000@users.noreply.github.com>
2020-02-15 20:42:29 -0500

    Update templatecreator.py

--- templatecreator.py
+++ templatecreator.py
@@ -6,9 +6,9 @@ driver = webdriver.Chrome(
 driver.get('https://docs.google.com/document/u/0/')
 driver.implicitly_wait(10)
 # inputs email
-driver.find_element_by_id('identifierId').send_keys("j.lu7000@gmail.com")
+driver.find_element_by_id('identifierId').send_keys("email")
 driver.find_element_by_id('identifierNext').click()
-driver.find_element_by_name('password').send_keys("Jl214755!")
+driver.find_element_by_name('password').send_keys("password")
 driver.find_element_by_id('passwordNext').click()
 
 print("shitbot")
